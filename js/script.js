64d1a8771d00ec44eee0e4bde144a3b91ef9ef5c
Michelle Tyson <michelle.tyson@verizon.com>
2019-06-07 11:05:35 -0500

    updated correct version

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
 // Michelle Tyson - Tech degree - Project 1
 
 
-/*Created a variable named quotes and inserted quotes in the array. */
+//Created a variable named quotes and inserted quotes in the array. 
 
 var quotes = [ 
   { 
     quote: "Tough times never last, but tough people do",
-    source: "Robert H. Schuller",           /* Added 'source' property  to quote object*/
-    citation: "Book",                       /* Added 'citatiom' property to quote object */
-    year: "1984"                            /* Added 'year' propert to quote object */
+    source: "Robert H. Schuller",           // Added 'source' property  to quote object
+    citation: "Book",                       // Added 'citatiom' property to quote object 
+    year: "1984"                            // Added 'year' propert to quote object 
   }, 
   { 
     quote: "Whoever does not love does not know God, because God is love.",
@@ -34,43 +34,30 @@ var quotes = [
 }
 ];
 
-console.log(quotes);
-
-
-/* random background color*/
-
-function random_bg_color() {
-  var x = Math.floor(Math.random() * 256);
-  var y = Math.floor(Math.random() * 256);
-  var z = Math.floor(Math.random() * 256);
-  var bgColor = "rgb(" + x + "," + y + "," + z + ")";
-console.log(bgColor);
-
-  document.body.style.background = bgColor;
-  }
-
-random_bg_color();
-
- 
-
-  /*Created a function named getRandomQuote. */
+  // Created a function named getRandomQuote. 
 
   function getRandomQuote(array) {
-    var RandomNumber = Math.floor( Math.random() * quotes.length);      /* Created a Random number generator */
-    var randomQuote = array[RandomNumber];                             /* Created a Random quote variable */
-    
-    return quotes[RandomNumber];                                      /* Return quote */
+    var RandomNumber = Math.floor( Math.random() * quotes.length);      // Random number generator 
+    return quotes[RandomNumber];                                      // Return quote
+  };
+  
+
+  
+// random background color fuction
+
+  function bg_color_random()
+  {
+  var randomColor = '#' + Math.floor(Math.random()*16777215).toString(16);
+    document.body.style.backgroundColor = randomColor;
   }
 
-  console.log(getRandomQuote);
-
-
-  /* Create a function named printQuote */
+  // printQuote function
 
   function printQuote() {
-    var result = getRandomQuote(quotes);                            //Calls the getRandomQuote function */
-    var message = "";                                              //Created a variable to set it to an empty string 
-    message = "<p class='quote'>" + result.quote + "</p>";        
+    bg_color_random();                                              // Calls random background color
+    var message = "";                                               // variable to set to an empty string 
+    var result = getRandomQuote(quotes);                            // Calls the getRandomQuote function 
+    message += "<p class='quote'>" + result.quote + "</p>";        
     message += "<p class='source'>" + result.source;
     if (result.citation) {
       message += "<span class='citation'>" + result.citation + "</span>";  // if condition is true show citation
@@ -79,22 +66,19 @@ random_bg_color();
       message += "<span class='year'>" + result.year + "</span>"
     }
     message += "</p>";
-    document.getElementById('quote-box').innerHTML = message;     /* Set inner HTML to the HTML string */
-
     
-    var result = random_bg_color()                              // Calls the random background colors
+    document.getElementById('quote-box').innerHTML = message;     // Set inner HTML to the HTML string 
+  }
 
-}
+    printQuote();                                                // calls the printQuote function
     
-  console.log(printQuote);
+    setInterval(function(){ printQuote(); }, 20000);             // Created function to change every 20 seconds
+   
+    document.getElementById('loadQuote').addEventListener("click", printQuote, false);  /* The event listener will be triggered and it will call, 
+                                                                                            or "invoke", the 'printQuote' function when the 
+                                                                                            "Show another quote" button is clicked.*/
 
 
-/*
-The event listener will be triggered and it will call, or "invoke", the 'printQuote' 
-function when the "Show another quote" button is clicked.
- */
-
-document.getElementById('loadQuote').addEventListener("click", printQuote, false);  
 
 
 
